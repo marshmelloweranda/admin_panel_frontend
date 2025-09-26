9c318052bfe500e3f173531cbb0343a72858ac05
marshmelloweranda <dasunw@pmu.mode.gov.lk>
2025-09-26 15:37:44 +0530

    project setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import AdminPanel from './AdminPanel';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+         <AdminPanel />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import AdminPanel from './AdminPanel';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <AdminPanel />
+  </React.StrictMode>
+);
+
+
